e101e7df39c55bb01738f5a8ff08618d78893efa
darwinpiodos <darwindivinopiodos21@gmail.com>
2024-06-25 21:53:24 +0800

    Recently I added to my dependencies: cors, cookie parser, I also made an error Handler after the logger in my middleWare by: darkpiodos on June 25, 2024 =Night time in the Philippines

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const PORT = process.env.PORT || 3500;
 
-app.use("/", express.static(path.join(__dirname, "/public")));
+app.use(logger);
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/root"));
 
 app.all("*", (req, res) => {
@@ -16,5 +24,6 @@ app.all("*", (req, res) => {
     res.type("txt").send("404 Not Found");
   }
 });
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
